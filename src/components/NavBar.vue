bfb75ff80bdfc090f2c759bc7dd6fa52704d6cd3
Kelvin Flora <kelvin.flora@gmail.com>
2023-03-29 17:35:15 -0300

    add NavBar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,43 @@
+<template>
+    <nav class="navbar bg-dark">
+  <div class="container">
+    <a class="navbar-brand text-light" href="#">
+      <img src="./icons/IconPsycho.png" alt="Logo" width="35" height="29" class="d-inline-block align-text-top">
+      Marciran Brisida
+    </a>
+    <ul class="nav justify-content-center menu-text">
+  <li class="nav-item">
+    <a class="nav-link active" aria-current="page" href="#">Início</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Sobre</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Serviços</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Contato</a>
+  </li>
+</ul>
+  </div>
+</nav>
+</template>
+
+<script>
+    export default {
+        name: 'NavBar',
+        }
+</script>
+
+<style>
+.nav-link {
+    color: white;
+}
+.navbar-nav > .active > a {
+  color: rgb(253, 210, 16);
+  background-color: rgb(253, 210, 16);    
+}
+.nav-item > a:hover {
+  color: rgb(253, 210, 16);
+}
+</style>
