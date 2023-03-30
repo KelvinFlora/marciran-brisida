0caff4b80997008fe4d1bc5ea1ae78894f67b053
Kelvin Flora <kelvin.flora@gmail.com>
2023-03-30 14:28:32 -0700

    view create

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup>
-import TheWelcome from "../components/TheWelcome.vue";
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab ex voluptate incidunt at deleniti ut commodi eos suscipit, odio nam earum temporibus iure illum architecto totam, exercitationem a? Iusto, ipsam!</h1>
+    <h2>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolor officiis quos quibusdam ipsam magni delectus exercitationem deserunt neque pariatur unde deleniti ratione, molestias asperiores atque, vel ut quae nam fugit.</h2>
+    <h2>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolor officiis quos quibusdam ipsam magni delectus exercitationem deserunt neque pariatur unde deleniti ratione, molestias asperiores atque, vel ut quae nam fugit.</h2>
+
   </main>
 </template>
