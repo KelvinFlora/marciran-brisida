67ce549a5efe22812140dd8d96ec7ba522373ac1
Kelvin Flora <kelvin.flora@gmail.com>
2023-03-31 19:54:36 -0300

    new content in homepage

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,9 +3,38 @@
 
 <template>
   <main>
-    <h1>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab ex voluptate incidunt at deleniti ut commodi eos suscipit, odio nam earum temporibus iure illum architecto totam, exercitationem a? Iusto, ipsam!</h1>
-    <h2>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolor officiis quos quibusdam ipsam magni delectus exercitationem deserunt neque pariatur unde deleniti ratione, molestias asperiores atque, vel ut quae nam fugit.</h2>
-    <h2>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolor officiis quos quibusdam ipsam magni delectus exercitationem deserunt neque pariatur unde deleniti ratione, molestias asperiores atque, vel ut quae nam fugit.</h2>
-
+    <!-- Image -->
+    <!-- Quote -->
+    <div class="container-fluid text-center pt-5 pb-5 bg-dark quote-bg">
+  <h1 class="home-quote text-light">"A mente é como um jardim. Se não for cultivada, pode se tornar uma selva." - Desmond Tutu</h1>
+    </div>
+    <div class="container text-center">
+      <img class="pt-4 photo-profile" alt="Foto de perfil do Marciran" src="../../public/images/marciran-photo-prof.png" />
+    </div>
+    <div class="container text-center pt-3">
+      <h2>Sobre mim</h2>
+    </div>
+    <div class="container text-center pt-4">
+      <h4 class="text-about">Meu nome é  <strong>Marciran Eleuterio Brisida</strong>, Sou <strong>Psicólogo Clínico</strong> (CRP 04/66679), atuo na abordagem de <strong>Terapia Cognitivo Comportamental</strong>.
+        O foco do meu atendimento são questões que trazem sofrimento emocional para o indivíduo. Atendo crianças, adolescentes, adultos e idosos.
+        Tenho especialização em terapia cognitivo comportamental. Meu conhecimento e experiência pode auxiliar você a saber lidar com suas emoções e
+        sentimentos disfuncionais.</h4>
+    </div>
   </main>
 </template>
+<style>
+.home-quote {
+  font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+  font-size: 150%;
+}
+.quote-bg {
+  
+}
+.photo-profile {
+  align-items: center;
+  width: 20%;
+}
+.text-about {
+  font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+}
+</style>
