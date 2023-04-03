86129d665a792f03ddaf2b6b47def63a18679e6c
Kelvin Flora <kelvin.flora@gmail.com>
2023-04-03 19:05:12 -0300

    add about content in about page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,21 @@
 <template>
-  <div class="about">
-    <h1>Sobre</h1>
+  <div class="container text-center">
+      <img class="pt-4 photo-profile" alt="Foto de perfil do Marciran" src="../../public/images/marciran-photo-prof.png" />
+    </div>
+  <div class="container text-center pt-3">
+      <h2>Sobre mim</h2>
+    </div>
+    <div class="container-fluid text-center pt-4">
+      <div class="row justify-content-center">
+      <h4 class="col-8 align-self-center text-about">Meu nome é <strong>Marciran Eleuterio Brisida</strong>, Sou <strong>Psicólogo Clínico</strong>. Atendo crianças, adolescentes, adultos e idosos.
+        <br>
+        Formado em <strong>Psicologia</strong> pela Universidade José do Rosário Vellano - UNIFENAS.
+        <br>
+        Especialista em <strong>Terapia Cognitivo-Comportamental</strong> pela Pontifícia Universidade Católica do Paraná - PUCPR.
+        <br>
+        Psicólogo credenciado junto ao Conselho Federal de Psicologia - CFP para <strong>atendimento online</strong> segundo a Resolução CFP Nº: 11/2018.
+      </h4>
+    </div>
   </div>
 </template>
 
