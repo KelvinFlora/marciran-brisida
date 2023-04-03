eb597a108edd56128ff8ff5e1d1a7c599ff74dc8
Kelvin Flora <kelvin.flora@gmail.com>
2023-04-03 19:04:27 -0300

    The Psychotherapy and Services layout

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,23 +3,80 @@
 
 <template>
   <main>
-    <!-- Image -->
+    <!-- Initial text -->
+    <div class="container pt-4 pb-4">
+      <div class="row align-self-xl-center">
+      <h1 class="">A Psicoterapia</h1>
+      <h2 class="col-xl-6 pt-2 psycho-text">Muitas pessoas ainda têm dúvidas sobre a psicoterapia e acham que é algo reservado apenas para quem tem "problemas graves".
+        <br>
+        No entanto, isso não é verdade. A terapia pode ser útil para qualquer pessoa que esteja enfrentando dificuldades na vida e que queira
+        melhorar sua saúde mental e bem-estar geral.
+        <br>
+        É importante lembrar que buscar ajuda não é um sinal de fraqueza, mas sim de coragem e resiliência. Todos nós enfrentamos dificuldades
+         em algum momento da vida, e buscar ajuda pode ser uma forma de enfrentar esses desafios de forma mais eficaz.
+         <br>
+        Se você está lutando contra problemas de saúde mental, ou se sabe de alguém que está enfrentando esses desafios, considere a 
+        possibilidade de buscar ajuda profissional. A psicoterapia pode ser uma ferramenta valiosa para ajudá-lo a lidar com esses problemas 
+        e melhorar sua qualidade de vida.</h2>
+        <div class="col-xl-6 text-center">
+          <img class="rounded w-75" src="../../public/images/yoga-g336800fd7_1280.jpg" alt="Pessoa meditando">
+        </div>
+    </div>
+  </div>
     <!-- Quote -->
     <div class="container-fluid text-center pt-5 pb-5 bg-dark quote-bg">
   <h1 class="home-quote text-light">"A mente é como um jardim. Se não for cultivada, pode se tornar uma selva." - Desmond Tutu</h1>
     </div>
     <div class="container text-center">
-      <img class="pt-4 photo-profile" alt="Foto de perfil do Marciran" src="../../public/images/marciran-photo-prof.png" />
+      <img class="pt-4 photo-profile" alt="Foto de perfil do Psicólogo Marciran" src="../../public/images/marciran-photo-prof.png" />
     </div>
     <div class="container text-center pt-3">
       <h2>Sobre mim</h2>
     </div>
-    <div class="container text-center pt-4">
-      <h4 class="text-about">Meu nome é  <strong>Marciran Eleuterio Brisida</strong>, Sou <strong>Psicólogo Clínico</strong> (CRP 04/66679), atuo na abordagem de <strong>Terapia Cognitivo Comportamental</strong>.
-        O foco do meu atendimento são questões que trazem sofrimento emocional para o indivíduo. Atendo crianças, adolescentes, adultos e idosos.
-        Tenho especialização em terapia cognitivo comportamental. Meu conhecimento e experiência pode auxiliar você a saber lidar com suas emoções e
-        sentimentos disfuncionais.</h4>
+    <div class="container-fluid text-center pt-4">
+      <div class="row justify-content-center">
+      <h4 class="col-8 align-self-center text-about">Meu nome é <strong>Marciran Eleuterio Brisida</strong>, Sou <strong>Psicólogo Clínico</strong>. Atendo crianças, adolescentes, adultos e idosos.
+        <br>
+        Formado em <strong>Psicologia</strong> pela Universidade José do Rosário Vellano - UNIFENAS.
+        <br>
+        Especialista em <strong>Terapia Cognitivo-Comportamental</strong> pela Pontifícia Universidade Católica do Paraná - PUCPR.
+        <br>
+        Psicólogo credenciado junto ao Conselho Federal de Psicologia - CFP para <strong>atendimento online</strong> segundo a Resolução CFP Nº: 11/2018.
+      </h4>
     </div>
+  </div>
+  <!--         
+    O foco do meu atendimento está voltado para questões relacionadas ao sofrimento emocional no indivíduo. Atendo crianças, adolescentes, adultos e idosos.
+    Meu conhecimento e experiência pode auxiliar você a compreender e lidar com suas emoções e pensamentos.
+      -->
+
+      <!-- Serviços -->
+      <div class="container-fluid pt-4 pb-4 text-center bg-dark text-white">
+        <h1 class="pb-3">Serviços</h1>
+        <div class="card-group">
+  <div class="card bg-dark">
+    <img src="../../public/images/psychotherapy.jpg" class="card-img-top w-50" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Atendimento Psicológico</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+    </div>
+  </div>
+  <div class="card bg-dark">
+    <img src="../../public/images/parents-orientation.jpeg" class="card-img-top w-50" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Orientação de Pais</h5>
+      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+    </div>
+  </div>
+  <div class="card bg-dark">
+    <img src="../../public/images/couple-therapy.jpg" class="card-img-top w-50" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Terapia de Casal</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+    </div>
+  </div>
+</div>
+      </div>
   </main>
 </template>
 <style>
@@ -36,5 +93,15 @@
 }
 .text-about {
   font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+  font-size: larger;
 }
+.psycho-text {
+  font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+  font-size: larger;
+}
+.h3 {
+  font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+}
+
+
 </style>
