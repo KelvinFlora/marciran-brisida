68b4e7fe8e7acbdd38af1a3991f6f4da2944798f
Kelvin Flora <kelvin.flora@gmail.com>
2023-04-03 19:13:28 -0300

    add content in services page

--- src/views/ServicesView.vue
+++ src/views/ServicesView.vue
@@ -1,3 +1,28 @@
 <template>
-    <h1>Serviços</h1>
+    <div class="container-fluid pt-4 pb-4 text-center">
+        <h1 class="pb-3">Serviços</h1>
+        <div class="card-group">
+  <div class="card">
+    <img src="../../public/images/psychotherapy.jpg" class="card-img-top w-50" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Atendimento Psicológico</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+    </div>
+  </div>
+  <div class="card">
+    <img src="../../public/images/parents-orientation.jpeg" class="card-img-top w-50" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Orientação de Pais</h5>
+      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+    </div>
+  </div>
+  <div class="card">
+    <img src="../../public/images/couple-therapy.jpg" class="card-img-top w-50" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Terapia de Casal</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+    </div>
+  </div>
+</div>
+      </div>
 </template>
