578e2f61b5c000214668755008a96fdc4621d313
Kelvin Flora <kelvin.flora@gmail.com>
2023-04-04 12:13:14 -0300

    card fixes

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,61 +1,66 @@
 <template>
-    <nav class="navbar navbar-expand-lg bg-dark">
-  <div class="container">
-    <a class="navbar-brand text-light" href="#">
-      <img src="./icons/IconPsychology.png" alt="Logo" width="35" height="29" class="d-inline-block align-text-top">
-      Marciran Brisida
-    </a>
-    <button
-            class="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="nav justify-content-center">
-  <li class="nav-item">
-    <router-link class="nav-link" to="/">Início</router-link>
-  </li>
-  <li class="nav-item">
-    <router-link class="nav-link" to="/sobre">Sobre</router-link>
-  </li>
-  <li class="nav-item">
-    <router-link class="nav-link" to="/serviços">Serviços</router-link>
-  </li>
-  <li class="nav-item">
-    <router-link class="nav-link" to="/contato">Contato</router-link>
-  </li>
-</ul>
-  </div>
-  </div>
-</nav>
+  <nav class="navbar navbar-expand-lg bg-dark">
+    <div class="container">
+      <a class="navbar-brand text-light" href="#">
+        <img
+          src="./icons/IconPsychology.png"
+          alt="Logo"
+          width="35"
+          height="29"
+          class="d-inline-block align-text-top"
+        />
+        Marciran Brisida
+      </a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="nav justify-content-center">
+          <li class="nav-item">
+            <router-link class="nav-link" to="/">Início</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/sobre">Sobre</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/serviços">Serviços</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/contato">Contato</router-link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
 </template>
 
 <script>
-    export default {
-        name: 'NavBar',
-        }
+export default {
+  name: "NavBar"
+};
 </script>
 
 <style>
 .nav-link {
-    color: white;
+  color: white;
 }
 .navbar-nav > .active > a {
-  color: rgb(253, 210, 16);
-  background-color: rgb(253, 210, 16);    
+  color: var(--bs-primary);
+  /* color: rgb(253, 210, 16); */
 }
 .nav-item > a:hover {
-  color: rgb(253, 210, 16);
+  color: var(--bs-primary);
 }
 
 .navbar .navbar-toggler-icon {
   background-image: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 30 30'%3e%3cpath stroke='rgba%28255, 255, 255' stroke-linecap='round' stroke-miterlimit='10' stroke-width='2' d='M4 7h22M4 15h22M4 23h22'/%3e%3c/svg%3e");
 }
-
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,107 +1,135 @@
-<script setup>
-</script>
+<script setup></script>
 
 <template>
   <main>
     <!-- Initial text -->
     <div class="container pt-4 pb-4">
       <div class="row align-self-xl-center">
-      <h1 class="">A Psicoterapia</h1>
-      <h2 class="col-xl-6 pt-2 psycho-text">Muitas pessoas ainda têm dúvidas sobre a psicoterapia e acham que é algo reservado apenas para quem tem "problemas graves".
-        <br>
-        No entanto, isso não é verdade. A terapia pode ser útil para qualquer pessoa que esteja enfrentando dificuldades na vida e que queira
-        melhorar sua saúde mental e bem-estar geral.
-        <br>
-        É importante lembrar que buscar ajuda não é um sinal de fraqueza, mas sim de coragem e resiliência. Todos nós enfrentamos dificuldades
-         em algum momento da vida, e buscar ajuda pode ser uma forma de enfrentar esses desafios de forma mais eficaz.
-         <br>
-        Se você está lutando contra problemas de saúde mental, ou se sabe de alguém que está enfrentando esses desafios, considere a 
-        possibilidade de buscar ajuda profissional. A psicoterapia pode ser uma ferramenta valiosa para ajudá-lo a lidar com esses problemas 
-        e melhorar sua qualidade de vida.</h2>
+        <h1 class="">A Psicoterapia</h1>
+        <h2 class="col-xl-6 pt-2 psycho-text">
+          Muitas pessoas ainda têm dúvidas sobre a psicoterapia e acham que é algo reservado apenas
+          para quem tem "problemas graves".
+          <br />
+          No entanto, isso não é verdade. A terapia pode ser útil para qualquer pessoa que esteja
+          enfrentando dificuldades na vida e que queira melhorar sua saúde mental e bem-estar geral.
+          <br />
+          É importante lembrar que buscar ajuda não é um sinal de fraqueza, mas sim de coragem e
+          resiliência. Todos nós enfrentamos dificuldades em algum momento da vida, e buscar ajuda
+          pode ser uma forma de enfrentar esses desafios de forma mais eficaz.
+          <br />
+          Se você está lutando contra problemas de saúde mental, ou se sabe de alguém que está
+          enfrentando esses desafios, considere a possibilidade de buscar ajuda profissional. A
+          psicoterapia pode ser uma ferramenta valiosa para ajudá-lo a lidar com esses problemas e
+          melhorar sua qualidade de vida.
+        </h2>
         <div class="col-xl-6 text-center">
-          <img class="rounded w-75" src="../../public/images/yoga-g336800fd7_1280.jpg" alt="Pessoa meditando">
+          <img
+            class="rounded w-75"
+            src="../../public/images/yoga-g336800fd7_1280.jpg"
+            alt="Pessoa meditando"
+          />
         </div>
+      </div>
     </div>
-  </div>
     <!-- Quote -->
     <div class="container-fluid text-center pt-5 pb-5 bg-dark quote-bg">
-  <h1 class="home-quote text-light">"A mente é como um jardim. Se não for cultivada, pode se tornar uma selva." - Desmond Tutu</h1>
+      <h1 class="home-quote text-light">
+        "A mente é como um jardim. Se não for cultivada, pode se tornar uma selva." - Desmond Tutu
+      </h1>
     </div>
     <div class="container text-center">
-      <img class="pt-4 photo-profile" alt="Foto de perfil do Psicólogo Marciran" src="../../public/images/marciran-photo-prof.png" />
+      <img
+        class="pt-4 photo-profile"
+        alt="Foto de perfil do Psicólogo Marciran"
+        src="../../public/images/marciran-photo-prof.png"
+      />
     </div>
     <div class="container text-center pt-3">
       <h2>Sobre mim</h2>
     </div>
-    <div class="container-fluid text-center pt-4">
+    <div class="container-fluid text-center pt-4 pb-4">
       <div class="row justify-content-center">
-      <h4 class="col-8 align-self-center text-about">Meu nome é <strong>Marciran Eleuterio Brisida</strong>, Sou <strong>Psicólogo Clínico</strong>. Atendo crianças, adolescentes, adultos e idosos.
-        <br>
-        Formado em <strong>Psicologia</strong> pela Universidade José do Rosário Vellano - UNIFENAS.
-        <br>
-        Especialista em <strong>Terapia Cognitivo-Comportamental</strong> pela Pontifícia Universidade Católica do Paraná - PUCPR.
-        <br>
-        Psicólogo credenciado junto ao Conselho Federal de Psicologia - CFP para <strong>atendimento online</strong> segundo a Resolução CFP Nº: 11/2018.
-      </h4>
+        <h4 class="col-8 align-self-center text-about">
+          Meu nome é <strong>Marciran Eleuterio Brisida</strong>, Sou
+          <strong>Psicólogo Clínico</strong>. Atendo crianças, adolescentes, adultos e idosos.
+          <br />
+          Formado em <strong>Psicologia</strong> pela Universidade José do Rosário Vellano -
+          UNIFENAS.
+          <br />
+          Especialista em <strong>Terapia Cognitivo-Comportamental</strong> pela Pontifícia
+          Universidade Católica do Paraná - PUCPR.
+          <br />
+          Psicólogo credenciado junto ao Conselho Federal de Psicologia - CFP para
+          <strong>atendimento online</strong> segundo a Resolução CFP Nº: 11/2018.
+        </h4>
+      </div>
     </div>
-  </div>
-  <!--         
+    <!--         
     O foco do meu atendimento está voltado para questões relacionadas ao sofrimento emocional no indivíduo. Atendo crianças, adolescentes, adultos e idosos.
     Meu conhecimento e experiência pode auxiliar você a compreender e lidar com suas emoções e pensamentos.
       -->
 
-      <!-- Serviços -->
-      <div class="container-fluid pt-4 pb-4 text-center bg-dark text-white">
-        <h1 class="pb-3">Serviços</h1>
-        <div class="card-group">
-  <div class="card bg-dark">
-    <img src="../../public/images/psychotherapy.jpg" class="card-img-top w-50" alt="...">
-    <div class="card-body">
-      <h5 class="card-title">Atendimento Psicológico</h5>
-      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-    </div>
-  </div>
-  <div class="card bg-dark">
-    <img src="../../public/images/parents-orientation.jpeg" class="card-img-top w-50" alt="...">
-    <div class="card-body">
-      <h5 class="card-title">Orientação de Pais</h5>
-      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-    </div>
-  </div>
-  <div class="card bg-dark">
-    <img src="../../public/images/couple-therapy.jpg" class="card-img-top w-50" alt="...">
-    <div class="card-body">
-      <h5 class="card-title">Terapia de Casal</h5>
-      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-    </div>
-  </div>
-</div>
+    <!-- Serviços -->
+    <div class="container-fluid pt-4 pb-4 text-center bg-dark text-white">
+      <h1 class="pb-3">Serviços</h1>
+      <div class="card-group">
+        <div class="card bg-dark align-items-center">
+          <img src="../../public/images/psychotherapy.jpg" class="card-img-top w-50" alt="..." />
+          <div class="card-body">
+            <h5 class="card-title">Atendimento Psicológico</h5>
+            <p class="card-text">
+              This is a wider card with supporting text below as a natural lead-in to additional
+              content. This content is a little bit longer.
+            </p>
+          </div>
+        </div>
+        <div class="card bg-dark align-items-center">
+          <img
+            src="../../public/images/parents-orientation.jpeg"
+            class="card-img-top w-50"
+            alt="..."
+          />
+          <div class="card-body">
+            <h5 class="card-title">Orientação de Pais</h5>
+            <p class="card-text">
+              This card has supporting text below as a natural lead-in to additional content.
+            </p>
+          </div>
+        </div>
+        <div class="card bg-dark align-items-center">
+          <img src="../../public/images/couple-therapy.jpg" class="card-img-top w-50" alt="..." />
+          <div class="card-body">
+            <h5 class="card-title">Terapia de Casal</h5>
+            <p class="card-text">
+              This is a wider card with supporting text below as a natural lead-in to additional
+              content. This card has even longer content than the first to show that equal height
+              action.
+            </p>
+          </div>
+        </div>
       </div>
+    </div>
   </main>
 </template>
 <style>
 .home-quote {
-  font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+  font-family: "Gill Sans", "Gill Sans MT", Calibri, "Trebuchet MS", sans-serif;
   font-size: 150%;
 }
-.quote-bg {
-  
-}
+
 .photo-profile {
   align-items: center;
   width: 20%;
 }
 .text-about {
-  font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+  font-family: "Gill Sans", "Gill Sans MT", Calibri, "Trebuchet MS", sans-serif;
   font-size: larger;
 }
 .psycho-text {
-  font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+  font-family: "Gill Sans", "Gill Sans MT", Calibri, "Trebuchet MS", sans-serif;
   font-size: larger;
 }
 .h3 {
-  font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+  font-family: "Gill Sans", "Gill Sans MT", Calibri, "Trebuchet MS", sans-serif;
 }
-
-
 </style>
