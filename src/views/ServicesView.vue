072985d3bfffc90ce4e3c23cdf3871143699922a
Kelvin Flora <kelvin.flora@gmail.com>
2023-04-05 10:13:31 -0300

    edit content in card

--- src/views/ServicesView.vue
+++ src/views/ServicesView.vue
@@ -1,28 +1,42 @@
 <template>
-    <div class="container-fluid pt-4 pb-4 text-center">
-        <h1 class="pb-3">Serviços</h1>
-        <div class="card-group">
-  <div class="card">
-    <img src="../../public/images/psychotherapy.jpg" class="card-img-top w-50" alt="...">
-    <div class="card-body">
-      <h5 class="card-title">Atendimento Psicológico</h5>
-      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-    </div>
-  </div>
-  <div class="card">
-    <img src="../../public/images/parents-orientation.jpeg" class="card-img-top w-50" alt="...">
-    <div class="card-body">
-      <h5 class="card-title">Orientação de Pais</h5>
-      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-    </div>
-  </div>
-  <div class="card">
-    <img src="../../public/images/couple-therapy.jpg" class="card-img-top w-50" alt="...">
-    <div class="card-body">
-      <h5 class="card-title">Terapia de Casal</h5>
-      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-    </div>
-  </div>
-</div>
+  <div class="container-fluid pt-4 pb-4 text-center">
+    <h1 class="pb-3">Serviços</h1>
+    <div class="card-group">
+      <div class="card align-items-center">
+        <img src="../../public/images/psychotherapy.jpg" class="card-img-top w-50" alt="..." />
+        <div class="card-body">
+          <h5 class="card-title">Atendimento Psicológico</h5>
+          <p class="card-text">
+            Atendimentos Psicológicos voltados para todos os públicos (Infantil, Adolescente, Adulto
+            e Idoso).
+          </p>
+        </div>
       </div>
+      <div class="card align-items-center">
+        <img
+          src="../../public/images/parents-orientation.jpeg"
+          class="card-img-top w-50"
+          alt="..."
+        />
+        <div class="card-body">
+          <h5 class="card-title">Orientação de Pais</h5>
+          <p class="card-text">
+            Orientações aos pais relacionadas a comportamentos e como lidar com situação adversas
+            nos diversos ambientes onde seus filhos estão inseridos.
+          </p>
+        </div>
+      </div>
+      <div class="card align-items-center">
+        <img src="../../public/images/couple-therapy.jpg" class="card-img-top w-50" alt="..." />
+        <div class="card-body">
+          <h5 class="card-title">Terapia de Casal</h5>
+          <p class="card-text">
+            A terapia de casal possibilitará a identificação e discussão de questões que estão
+            afetando o relacionamento, como problemas de comunicação, conflitos, falta de
+            intimidade, infidelidade ou outras dificuldades.
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
