04e5a4a682c2e7352a08ecd0bad340b576fa2103
Kelvin Flora <kelvin.flora@gmail.com>
2023-04-05 10:10:59 -0300

    add style on footer buttons

--- src/components/FooterSite.vue
+++ src/components/FooterSite.vue
@@ -1,61 +1,73 @@
 <template>
-        <!-- Footer -->
-        <footer class="bg-dark text-center text-white mt-auto">
-      <!-- Grid container -->
-      <div class="container p-4 pb-0">
-        <!-- Section: Social media -->
-        <section class="mb-4">
-          <!-- Facebook -->
-          <a
-            class="btn text-light btn-floating m-1 facebook-button"
-            href="https://www.facebook.com/marciran.brisida"
-            target="_blank"
-            role="button"
-          >
-            <i class="bi bi-facebook"></i
-          ></a>
+  <!-- Footer -->
+  <footer class="bg-dark text-center text-white mt-auto">
+    <!-- Grid container -->
+    <div class="container p-4 pb-0">
+      <!-- Section: Social media -->
+      <section class="mb-4">
+        <!-- Facebook -->
+        <a
+          class="btn text-light btn-floating m-1"
+          href="https://www.facebook.com/marciran.brisida"
+          target="_blank"
+          role="button"
+        >
+          <i class="bi bi-facebook facebook-button"></i
+        ></a>
 
-          <!-- Instagram -->
-          <a
-            class="btn text-light btn-floating m-1 instagram-button"
-            href="https://www.instagram.com/psi.marciranbrisida"
-            target="_blank"
-            role="button"
-            ><i class="bi bi-instagram"></i
-          ></a>
+        <!-- Instagram -->
+        <a
+          class="btn text-light btn-floating m-1"
+          href="https://www.instagram.com/psi.marciranbrisida"
+          target="_blank"
+          role="button"
+          ><i class="bi bi-instagram instagram-button"></i
+        ></a>
 
-          <!-- WhatsApp -->
-          <a
-            class="btn text-light btn-floating m-1 whatsapp-button"
-            href="https://wa.me/+5535988434758"
-            target="_blank"
-            role="button"
-            ><i class="bi bi-whatsapp"></i
-          ></a>
+        <!-- WhatsApp -->
+        <a
+          class="btn text-light btn-floating m-1"
+          href="https://wa.me/+5535988434758"
+          target="_blank"
+          role="button"
+          ><i class="bi bi-whatsapp whatsapp-button"></i
+        ></a>
 
-          <!-- Email (precisa colocar link) -->
-          <a
-            class="btn text-light btn-floating m-1 email-button"
-            href="#!"
-            target="_blank"
-            role="button"
-            ><i class="bi bi-envelope"></i
-          ></a>
-        </section>
-        <!-- Section: Social media -->
-      </div>
-      <!-- Grid container -->
+        <!-- Email (precisa colocar link) -->
+        <a class="btn text-light btn-floating m-1" href="#!" target="_blank" role="button"
+          ><i class="bi bi-envelope email-button"></i
+        ></a>
+      </section>
+      <!-- Section: Social media -->
+    </div>
+    <!-- Grid container -->
 
-      <!-- Copyright -->
-      <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2)">
-        © 2023 Marciran Brisida - Todos os direitos reservados | Desenvolvido por
-        Kelvin Flora
-      </div>
-      <!-- Copyright -->
-    </footer>
+    <!-- Copyright -->
+    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2)">
+      © 2023 Marciran Brisida - Todos os direitos reservados | Desenvolvido por Kelvin Flora
+    </div>
+    <!-- Copyright -->
+  </footer>
 </template>
 <script>
-    export default {
-        name: 'FooterSite'
-    }
+export default {
+  name: "FooterSite"
+};
 </script>
+<style>
+.facebook-button:hover {
+  color: rgb(253, 210, 16);
+}
+
+.instagram-button:hover {
+  color: rgb(253, 210, 16);
+}
+
+.whatsapp-button:hover {
+  color: rgb(253, 210, 16);
+}
+
+.email-button:hover {
+  color: rgb(253, 210, 16);
+}
+</style>
