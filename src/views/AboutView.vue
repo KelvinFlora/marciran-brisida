a2d69bdfcd4af418fd102e41a8f4bae1498d8840
Kelvin Flora <kelvin.flora@gmail.com>
2023-04-14 14:44:34 -0300

    mobile animations fixes

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -58,14 +58,20 @@
     margin-top: 0px;
     margin-bottom: 0px;
     width: 50%;
+    animation: fadeInDown;
+    animation-duration: 3s;
   }
   .about-title-p2 {
     margin-top: 0px;
     margin-bottom: 0px;
     font-size: 35px;
+    animation: fadeIn;
+    animation-duration: 5s;
   }
   .about-text-p2 {
     width: 100%;
+    animation: fadeInUp;
+    animation-duration: 3s;
   }
 }
 </style>
