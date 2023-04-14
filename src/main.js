50c68a20ddff8fbf84d5a85ca38d6a1d1e1f8163
Kelvin Flora <kelvin.flora@gmail.com>
2023-04-14 16:31:03 -0300

    fix: animate.css imports

--- src/main.js
+++ src/main.js
@@ -2,10 +2,9 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 
-import "animate.css";
-import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap/dist/js/bootstrap.js";
 import "./assets/main.scss";
+
+import "bootstrap/dist/js/bootstrap.js";
 
 const app = createApp(App);
 
